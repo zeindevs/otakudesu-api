e7296e7198f018566af85e5bcb77913b660a3f29
zeindevs <zeindevs@gmail.com>
2024-03-15 21:44:15 +0700

    fix: base url and error unused value

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,4 @@
-export const config = {
-  /* Base URL */
-  BASE_URL: 'https://otakudesu.cloud',
-  ADMIN_AJAX_URL: 'https://otakudesu.cloud/wp-admin/admin-ajax.php',
+export const OtakudesuConfig = {
   /** User-Agent */
   UA_ANDROID: 'Otakudesu 177.0.0.30.119 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)',
   UA_WINDOWS: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
